e697f5293f2a61ff07bd9bac138ff03dff8b36e9
Vanderson Guidi <vando.lg@gmail.com>
2021-12-16 22:01:34 -0300

    Tela de login

---
+++ src/Guidis.PDV.WPF/LoginWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Guidis.PDV.WPF
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        public Boolean IsDarkTheme { get; set; }
+        private readonly PaletteHelper paletteHelper = new PaletteHelper();
+
+        private void ToggleTheme(object sender, RoutedEventArgs e)
+        {
+            ITheme theme = paletteHelper.GetTheme();
+
+            if (IsDarkTheme = theme.GetBaseTheme() == BaseTheme.Dark)
+            {
+                IsDarkTheme = false;
+                theme.SetBaseTheme(Theme.Light);
+            }
+            else
+            {
+                IsDarkTheme = true;
+                theme.SetBaseTheme(Theme.Dark);
+            }
+
+            paletteHelper.SetTheme(theme);
+
+            Application.Current.Properties.Add("DarkTheme", IsDarkTheme);
+        }
+
+        private void Sair(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+            DragMove();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            txtUsuario.Focus();
+        }
+    }
+}
---
+++ src/Guidis.PDV.WPF/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace Guidis.PDV.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var form = new LoginWindow();
+            form.Show();
+
+            this.Close();
+        }
+    }
+}
